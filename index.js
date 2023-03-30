35398ddcfcf710a21c8f1e839e7b3db097b09a48
Gilberto Medero <gjmedero@gmail.com>
2023-03-29 23:57:08 -0400

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function title(sentence) {
+  const words =  sentence.split(" ");
+  
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+
+  return words.join(' ');
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title);
 }
